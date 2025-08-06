13dd979c6df29cfac9106f25f28cd5475ad5717d
Saqib <saqib08011@gmail.com>
2025-08-06 22:33:09 +0530

    Fix: Correct import casing for FriendRequest model

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import FriendRequest from "../models/Friendrequest.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 export async function getRecommendedUsers(req,res) {
     try {
